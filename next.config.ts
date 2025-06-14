a45bdd5d334b0ffdc456fe72b2856b4f8e76f2ef
Snghun <climbsh83@gmail.com>
2025-06-14 23:14:38 +0900

    Simplify GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/zeronine-homepage',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  assetPrefix: '/zeronine-homepage/',
 };
 
 export default nextConfig;
